9ee62be4e3cf426c393da62eefe89c1b003779ee
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-12-08 15:49:00 +0900

    멤버 id 변수 수정

--- backend/demo/src/main/java/keepgoing/demo/domain/member/entity/Member.java
+++ backend/demo/src/main/java/keepgoing/demo/domain/member/entity/Member.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class Member {
-    private Long memberId;      // int -> Long (BIGINT 매핑)
+    private Long id;      // int -> Long (BIGINT 매핑)
     private String email;
     private String password;
     private String name;
